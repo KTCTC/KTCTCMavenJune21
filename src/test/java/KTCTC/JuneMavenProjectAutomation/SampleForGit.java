11fef0a9cf8ddb1a82dee7151630c1b5de6cd110
KTCTC <ktcorporatetrainingcentre@gmail.com>
2021-10-19 21:06:01 +0530

    Second git class commit

--- src/test/java/KTCTC/JuneMavenProjectAutomation/SampleForGit.java
+++ src/test/java/KTCTC/JuneMavenProjectAutomation/SampleForGit.java
@@ -3,6 +3,8 @@ package KTCTC.JuneMavenProjectAutomation;
 public class SampleForGit {
 	public static void main(String[] args) {
 		System.out.println("Hello");
+		
+		System.out.println("Adding second line");
 	}
 
 }
